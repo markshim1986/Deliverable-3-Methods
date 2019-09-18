165d0771606724839ea82ca8fc32fa9e19dce8ea
markshim1986 <mshim@mail.usf.eud>
2019-09-18 19:59:21 -0400

    This is a change

--- Deliverable 3 Methods/Program.cs
+++ Deliverable 3 Methods/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿// Author: Mark Shim    
+// Date: 9/18/2019
+// Comments: C# Console application that demonstrates a Method
+
+using System;
 
 namespace Deliverable_3_Methods
 {
     class Program
     {
+        // Declaring my method
+        public void Get_Name(string name)
+        {
+            Console.WriteLine("\n Hello " + name + "!");
+            Console.WriteLine("\n Press any key to exit the program");
+            Console.ReadKey(true);
+        }
+        // Calling my method by passing the arguments when the method is called
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program p = new Program();
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+            p.Get_Name(name);
         }
+           
+
     }
 }
